0fcf7ed1db5f1af3aaa4fbf440b606c96f3bce5b
Joshua Bantayan <31948724+joshbantayan@users.noreply.github.com>
2017-12-12 21:38:17 +0800

    Updated solutions.java
    added my own methods

--- Solutions.java
+++ Solutions.java
@@ -44,7 +44,55 @@ public class Solutions{
 	    }
 	    return count == 1? "YES":"NO";
 	}
-
-
-
+	
+	//submitted by Joshua Bantayan
+	public static void offices(){
+	    String city = kbd.next();
+            int count = 0;
+	    for (int i = 1; i < city.length(); i++)
+                if (city.charAt(i) == 'F' && city.charAt(i-1) == 'S') {
+		    count++;
+	        } else if (city.charAt(i) == 'S' && city.charAt(i-1) == 'F') {
+	            count--;
+			}
+	    System.out.println(count > 0 ? "YES" : "NO");
+	}
+	
+	public static void diary(){
+	    int n = Integer.parseInt(scan.nextLine());
+	    HashSet<String> setHash = new HashSet<String>();
+		for(int i = 0; i < n ; i++){
+	            String str = scan.nextLine();
+			
+		    if(setHash.contains(str)){
+		        System.out.println("YES");
+			} else {
+			    System.out.Println("NO");
+		            tHash.add(str);
+			}
+			
+		}
+	}
+	
+	public static void acm(){
+	    int n = 0;
+            int[] values = new int[6];
+	        for(int x = 0; x < 6; x++) {
+	            values[x] = scan.nextInt();
+		    n += values[x];
+		}
+		boolean flag = false;
+		l: for(int i = 1; i < 5; i++) {
+			for(int j = i + 1; j < 6; j++) {
+			    if(2*(values[0]+values[i]+values[j])==n) {
+			        System.out.println("YES");
+				flag = true;
+				break l;
+				}
+			}
+		}
+		if(!flag) {
+		    System.out.println("NO");
+		}
+	}
 }
